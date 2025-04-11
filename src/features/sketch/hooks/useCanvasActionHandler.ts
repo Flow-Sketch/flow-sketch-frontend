6844d9beeb676e42bf8ecd247375e8fde179c482
IceBear9028 <tlatms9028@gmail.com>
2025-04-12 02:59:25 +0900

    [Feat] Element 클릭선택기능 개선 #56 - 마우스 이벤트 상태 관리 로직 개선
    1. 마우스 이벤트 처리 로직 개선
      - 드래그 거리 기반 상태 전환 로직 추가 (10px 기준)
        - mouseMove 이벤트가 생각보다 잘 발생해서 의도치 않은 'select' 모드가 발생
        - `tempStartPoint` 상태를 추가, `moveEvent` 발생 시 두 점의 차이가 10px 미만이면 잘못된 이벤트 발생으로 처리
        - `useSelectElementManager` 에 있던 로직을 `useCanvasActionHandler` 로 옮김
    
    2. EditMode 타입 정의
      - EditMode 타입을 별도로 분리하여 타입 안정성 강화
      - 'multiSelect'를 'select'로 이름 변경
    
    3. `useSelectElementManager` 개선
      - tempStartPoint 상태 제거 및 dragBox로 통합
      - 거리기반 드래그체크 로직 `useCanvasActionHandler` 로 옮김

--- src/features/sketch/hooks/useCanvasActionHandler.ts
+++ src/features/sketch/hooks/useCanvasActionHandler.ts
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { TRANSFORM_CONTROL_CORNER_WIDTH } from '@/features/sketch/constants';
-import { isPointInOBB } from '@/shared/utils/collidingDetection';
+import { isPointInOBB, Point, vectorLength } from '@/shared/utils/collidingDetection';
 import {
   CreateElementMangerAction,
   DeleteManagerAction,
@@ -13,6 +13,8 @@ import {
   useRemoteManager,
 } from '@/features/sketch/hooks/index.ts';
 
+type EditMode = 'idle' | 'resize' | 'move' | 'moveReady' | 'select' | 'selectReady';
+
 export function useCanvasActionHandler(action: {
   viewAction: ViewManagerAction;
   selectAction: SelectManagerAction;
@@ -24,7 +26,8 @@ export function useCanvasActionHandler(action: {
 }) {
   const { remoteState } = useRemoteManager();
   const { selectState } = useSelectElementManager();
-  const [editMode, setEditMode] = useState<'idle' | 'resize' | 'move' | 'moveReady' | 'selectReady' | 'multiSelect'>('idle');
+  const [editMode, setEditMode] = useState<EditMode>('idle');
+  const [tempStartPoint, setTempStartPoint] = useState<null | Point>(null); // 마우스 down 시 잘못클릭한 것인지 파악하기 위한 용도
 
   const { viewAction, selectAction, createAction, deleteAction, moveAction, resizeAction, clipboardAction } = action;
   const { shapeType, remoteMode } = remoteState;
@@ -34,6 +37,7 @@ export function useCanvasActionHandler(action: {
   })();
 
   const handleMouseDown = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!event) return;
     if (remoteMode === 'view') return viewAction.handleMouseDown(event);
     if (remoteMode === 'edit') {
       if (shapeType) return createAction.handleMouseDown(event);
@@ -56,6 +60,9 @@ export function useCanvasActionHandler(action: {
         height: boundingBox.height - TRANSFORM_CONTROL_CORNER_WIDTH,
         rotation: 0,
       };
+
+      setTempStartPoint(point);
+
       if (isPointInOBB(outerBox, point)) {
         if (isPointInOBB(innerBox, point)) {
           setEditMode('moveReady');
@@ -70,16 +77,27 @@ export function useCanvasActionHandler(action: {
   };
 
   const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
-    if (event.buttons !== 1) return; // 마우스 좌클릭하고 있는 동안에만 핸들러 실행
+    if (event.buttons !== 1) return; // 이미 좌클릭을 한 상태에서만 동작
     if (remoteMode === 'view') return viewAction.handleMouseMove(event);
     if (remoteMode === 'edit') {
       if (shapeType) return createAction.handleMouseMove(event);
+
+      if (!tempStartPoint) return;
+      const deltaVector = {
+        x: event.nativeEvent.offsetX - tempStartPoint.x,
+        y: event.nativeEvent.offsetY - tempStartPoint.y,
+      };
+      const distanceToTempPoint = vectorLength(deltaVector);
+
+      // 두 사이의 거리가 10px 을 넘지 않으면 값을 업데이트 하지 않음
+      if (distanceToTempPoint > 10) {
+        if (editMode === 'moveReady') return setEditMode('move');
+        if (editMode === 'selectReady') return setEditMode('select');
+      }
+
       if (editMode === 'move') return moveAction.handleMouseMove(event);
       if (editMode === 'resize') return resizeAction.handleMouseMove(event);
-      if (editMode === 'multiSelect') return selectAction.handleMouseMove(event);
-
-      if (editMode === 'moveReady') return setEditMode('move');
-      if (editMode === 'selectReady') return setEditMode('multiSelect');
+      if (editMode === 'select') return selectAction.handleMouseMove(event);
     }
   };
 
@@ -88,7 +106,7 @@ export function useCanvasActionHandler(action: {
     if (remoteMode === 'view') return viewAction.handleMouseUp();
     if (remoteMode === 'edit') {
       if (shapeType) return createAction.handleMouseUp();
-      if (editMode === 'multiSelect') {
+      if (editMode === 'select') {
         selectAction.handleMouseUp();
       }
       if (editMode === 'resize') {
@@ -100,7 +118,9 @@ export function useCanvasActionHandler(action: {
       if (editMode === 'moveReady' || editMode === 'selectReady') {
         selectAction.handleSingleSelect(event);
       }
-      return setEditMode('idle');
+      setEditMode('idle');
+      setTempStartPoint(null);
+      return;
     }
   };
 
